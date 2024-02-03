91c7d0ec0504ff557e35cc23981d971ffd7c084b
mmarian <mihailmarian12@gmail.com>
2024-02-03 20:54:04 +0000

    start landing page

--- src/db/client.ts
+++ src/db/client.ts
@@ -7,7 +7,9 @@ import type { Hashtag } from "../lib/types";
 const queryClient = postgres(import.meta.env.DB_URL || "");
 export const db = drizzle(queryClient);
 
-export async function getAllHashtags(countryCode: string): Promise<Hashtag[]> {
+export async function getHashtagsForCountry(
+  countryCode: string
+): Promise<Hashtag[]> {
   const rows = await db
     .select()
     .from(hashtag)
@@ -19,12 +21,37 @@ export async function getAllHashtags(countryCode: string): Promise<Hashtag[]> {
     )
     .orderBy(hashtag.rank);
 
+  processRows(rows);
+
+  return rows;
+}
+
+export async function getAllHashtags(): Promise<Hashtag[]> {
+  const rows = await db
+    .select()
+    .from(hashtag)
+    .where(and(eq(hashtag.latestTrending, true)));
+
+  shuffleArray(rows);
+  const rowsToProcess = rows.slice(0, 10);
+
+  processRows(rowsToProcess);
+
+  return rowsToProcess;
+}
+
+function processRows(rows: Hashtag[]) {
   rows.forEach((row) => {
     row.trend = row.trend.map((point) => ({
       time: point.time,
       value: point.value * 100,
     }));
   });
+}
 
-  return rows;
+function shuffleArray(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
