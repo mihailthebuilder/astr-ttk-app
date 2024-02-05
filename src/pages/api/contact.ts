4b927f6cd8c5f679d23d6c7356d5b89c1ea42dce
mmarian <mihailmarian12@gmail.com>
2024-02-05 09:34:07 +0000

    better error message for contact

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -3,24 +3,27 @@ import type { ContactRequest } from "../../lib/types";
 import SgMail from "@sendgrid/mail";
 
 export const POST: APIRoute = async ({ request }) => {
+  const apiKey = import.meta.env.SENDGRID_API_KEY;
+  SgMail.setApiKey(apiKey);
+
+  const body = (await request.json()) as ContactRequest;
+  const text = `Email: ${body.email} . Message: ${body.message}`;
+
+  const to = import.meta.env.EMAIL_FROM;
+  const from = import.meta.env.EMAIL_TO;
+  const subject = "New ttk contact submission";
+
   try {
-    const apiKey = import.meta.env.SENDGRID_API_KEY;
-    SgMail.setApiKey(apiKey);
-
-    const body = (await request.json()) as ContactRequest;
-    const text = `Email: ${body.email} . Message: ${body.message}`;
-
-    const to = import.meta.env.EMAIL_FROM;
-    const from = import.meta.env.EMAIL_TO;
-    const subject = "New ttk contact submission";
-
     await SgMail.send({ to, from, subject, text });
-
     return new Response("", { status: 200 });
   } catch (e) {
     const error = e as Error;
 
-    console.error("error sending email: ", error.message, error.stack);
+    console.error(
+      `error sending email from ${body.email} with message ${body.message}: `,
+      error.message,
+      error.stack
+    );
     return new Response("", { status: 500 });
   }
 };
