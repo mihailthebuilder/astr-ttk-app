26d3d40df72ddb5f3f371a1b1c29eff5e2210b90
mmarian <mihailmarian12@gmail.com>
2024-02-05 00:42:43 +0000

    contact form

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,34 @@
+export default function ContactForm() {
+  return (
+    <form className="flex flex-col items-center">
+      <div className="mb-4 w-full">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Your email"
+          className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-600"
+          required
+        />
+      </div>
+
+      <div className="mb-4 w-full">
+        <textarea
+          id="message"
+          name="message"
+          rows={4}
+          placeholder="Your message"
+          className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-600"
+          required
+        ></textarea>
+      </div>
+
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
