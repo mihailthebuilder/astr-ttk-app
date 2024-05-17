d5eea5675e3f229139a3755bd2223bff12f16924
mmarian <mihailmarian12@gmail.com>
2024-05-17 09:00:27 +0100

    make site static for cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
   integrations: [tailwind(), react()],
 });
